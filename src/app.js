e6cffbc54df2011d3c59ea355f489b4b4d4c2cde
KirillDanProg <kirill2898v@gmail.com>
2024-01-21 20:50:00 +0300

    feat: app root with routes created

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
-"use strict";
+import { MainView } from "./views/main/main.js";
+class App {
+  routes = [{ route: "", view: MainView }];
+  appState = {
+    favorites: [],
+  };
 
-import "./app.css";
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    const view = this.routes.find((r) => r.route === window.location.hash).view;
+    this.currentView = new view(this.appState);
+    this.currentView.render();
+  }
+}
+
+new App();
