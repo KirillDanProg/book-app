bfb4a75ccdc5dbdae96bc3ca9fd12abeca5e80a3
KirillDanProg <kirill2898v@gmail.com>
2024-01-21 20:55:39 +0300

    add: abstract view and div component  class

---
+++ src/common/view.js
@@ -0,0 +1,14 @@
+export class AbstractView {
+  constructor() {
+    this.app = document.querySelector("#root");
+  }
+  setTitle(title) {
+    document.title = title;
+  }
+  render() {
+    return;
+  }
+  destroy() {
+    return;
+  }
+}
