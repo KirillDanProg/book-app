6951181d19e70dd89d4e0bf583027ae64024c0df
KirillDanProg <kirill2898v@gmail.com>
2024-01-28 20:27:50 +0300

    add: saveBookInfoToState listener added to cardsList

--- src/components/cardsList/cardsList.js
+++ src/components/cardsList/cardsList.js
@@ -14,9 +14,12 @@ export class CardsList extends DivComponent {
   }
 
   addBookToFavorites(event) {
-    const button = event.target.closest(".card__button");
-    if (button) {
-      const bookKey = button.getAttribute("data-id");
+    const card = event.target.closest(".card");
+    const isButton =
+      card &&
+      (event.target.id === "card_btn" || event.target.id === "card_icon");
+    if (isButton) {
+      const bookKey = card.getAttribute("data-id");
       const book = this.booksList.find((book) => book.key === bookKey);
       const isAlreadyFavorite = this.checkIfFavorites(bookKey);
       if (isAlreadyFavorite) {
@@ -31,12 +34,22 @@ export class CardsList extends DivComponent {
     this.appState.favorites.delete(bookKey);
   }
 
+  saveBookInfoToState(event) {
+    const card = event.target.closest(".card");
+    if (card && event.target.id === "card_link") {
+      const bookKey = card.getAttribute("data-id");
+      const book = this.booksList.find((book) => book.key === bookKey);
+      this.appState.currentBook = book;
+    }
+  }
+
   render() {
     this.element.classList.add("cardsList");
     this.booksList.forEach((book) => {
       this.element.append(new Card(this.appState, book).render());
     });
     this.element.addEventListener("click", this.addBookToFavorites.bind(this));
+    this.element.addEventListener("click", this.saveBookInfoToState.bind(this));
     return this.element;
   }
 }
