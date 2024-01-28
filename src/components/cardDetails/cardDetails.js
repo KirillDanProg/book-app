6c8b120d205eb298c49557be1072f4ba8d817e3f
KirillDanProg <kirill2898v@gmail.com>
2024-01-28 20:32:16 +0300

    add: generateTags method added, add/remove book from favorites listener added, styles

--- src/components/cardDetails/cardDetails.js
+++ src/components/cardDetails/cardDetails.js
@@ -12,49 +12,70 @@ export class CardDetails extends DivComponent {
     return this.appState.favorites.has(bookKey);
   }
 
-  addBookToFavorites(event) {
-    const button = event.target.closest(".card__button");
-    if (button) {
-      const bookKey = button.getAttribute("data-id");
-      const book = this.booksList.find((book) => book.key === bookKey);
-      const isAlreadyFavorite = this.checkIfFavorites(bookKey);
-      if (isAlreadyFavorite) {
-        this.removeBookFromFavorites(bookKey);
-        return;
-      }
-      this.appState.favorites.set(bookKey, book);
-    }
+  addBookToFavorites() {
+    const bookKey = this.bookDetails.key;
+    const book = this.appState.currentBook;
+    this.appState.favorites.set(bookKey, book);
   }
 
   removeBookFromFavorites(bookKey) {
     this.appState.favorites.delete(bookKey);
   }
 
+  generateTags(tagsArr) {
+    const cardTags = this.element.querySelector(".card__details_tags");
+    for (let i = 0; i < 6; i++) {
+      const tag = document.createElement("span");
+      tag.classList.add("card__details_tag");
+      tag.innerHTML = tagsArr[i];
+      cardTags.append(tag);
+    }
+  }
+
   render() {
+    const bookKey = this.bookDetails.key;
+    const isBookFavorite = this.checkIfFavorites(bookKey);
     const bookCoverImgSrc = this.bookDetails.covers
       ? `https://covers.openlibrary.org/b/id/${this.bookDetails.covers[0]}-M.jpg`
       : "src/static/default_book_cover.jpeg";
-    const category = this.bookDetails.subjects[0];
+    const category = this.appState.currentBook.subject_facet[0];
     const description =
       this.bookDetails.description.value || this.bookDetails.description;
     const publication = this.bookDetails.first_publish_date || "Не указано";
+    const authorName = this.appState.currentBook.author_name[0];
+    const pagesNumber = this.appState.currentBook.number_of_pages_median;
     this.element.classList.add("card__details");
     this.element.innerHTML = `
       <div class="card__details_img-wrapper">
         <img class="card__details_img" src="${bookCoverImgSrc}"/>
       </div>
       <div class="card__details_info">
-        <div class="card__details_info-author"></div>
-        <div class="card__details_info-category">Категория: ${category}</div>
-        <div class="card__details_info-publication">Первая публикация: ${publication}</div>
-        <div class="card__details_info-pages"></div>
+        <div class="card__details_info-author">Автор: <strong>${authorName}</strong></div>
+        <div class="card__details_info-category">Категория: <strong>${category}</strong></div>
+        <div class="card__details_info-publication">Первая публикация: <strong>${publication}</strong></div>
+        <div class="card__details_info-pages">Кол-во страниц: <strong>${pagesNumber}</strong></div>
+        <button class="card__details_button">
+          ${isBookFavorite ? "Убрать из избранного" : "В избранное"}
+        </button>
       </div>
       <div class="card__details_description">
-          <strong>Описание:</strong> 
-          <p>${description}</p>
-        </div>
-      <div class="card__details_tags"></div>
+        <strong>Описание:</strong> 
+        <p>${description}</p>
+      </div>
+      <div class="card__details_tags">
+        <strong>Теги:</strong> 
+      </div>
     `;
+    const button = this.element.querySelector(".card__details_button");
+    if (isBookFavorite) {
+      button.addEventListener(
+        "click",
+        this.removeBookFromFavorites.bind(this, bookKey)
+      );
+    } else {
+      button.addEventListener("click", this.addBookToFavorites.bind(this));
+    }
+    this.generateTags(this.bookDetails.subjects);
     return this.element;
   }
 }
