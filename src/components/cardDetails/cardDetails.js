0ae61e87baf0f63c07b05e8afc56fcce7a7aff3f
KirillDanProg <kirill2898v@gmail.com>
2024-01-30 19:25:28 +0300

    fix: ternary operators added when properties is undefined

--- src/components/cardDetails/cardDetails.js
+++ src/components/cardDetails/cardDetails.js
@@ -24,6 +24,10 @@ export class CardDetails extends DivComponent {
 
   generateTags(tagsArr) {
     const cardTags = this.element.querySelector(".card__details_tags");
+    if (!tagsArr) {
+      cardTags.innerHTML += "Не указано";
+      return;
+    }
     for (let i = 0; i < 6; i++) {
       const tag = document.createElement("span");
       tag.classList.add("card__details_tag");
@@ -38,12 +42,19 @@ export class CardDetails extends DivComponent {
     const bookCoverImgSrc = this.bookDetails.covers
       ? `https://covers.openlibrary.org/b/id/${this.bookDetails.covers[0]}-M.jpg`
       : "src/static/default_book_cover.jpeg";
-    const category = this.appState.currentBook.subject_facet[0];
-    const description =
-      this.bookDetails.description?.value || this.bookDetails.description;
+    const category = this.appState.currentBook
+      ? this.appState.currentBook.subject_facet[0]
+      : "Не указано";
+    const description = this.bookDetails.description
+      ? this.bookDetails.description.value || this.bookDetails.description
+      : "Не указано";
     const publication = this.bookDetails.first_publish_date || "Не указано";
-    const authorName = this.appState.currentBook.author_name[0];
-    const pagesNumber = this.appState.currentBook.number_of_pages_median;
+    const authorName = this.appState.currentBook
+      ? this.appState.currentBook.author_name[0]
+      : "Не указано";
+    const pagesNumber = this.appState.currentBook
+      ? this.appState.currentBook.number_of_pages_median
+      : "Не указано";
     this.element.classList.add("card__details");
     this.element.innerHTML = `
       <div class="card__details_img-wrapper">
